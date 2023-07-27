892d1df6037e983a43ae8c5208cb6d03b193a96a
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-07-27 16:48:27 +0100

    feat: add constants, interfaces, utils and queries from ui project

---
+++ src/queries.ts
@@ -0,0 +1,69 @@
+import { gql } from 'graphql-request';
+
+export const FIND_BY_TAGS = gql`
+  query FIND_BY_TAGS($tags: [TagFilter!], $first: Int!, $after: String) {
+    transactions(tags: $tags, first: $first, after: $after, sort: HEIGHT_DESC) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          tags {
+            name
+            value
+          }
+          owner {
+            address
+            key
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_TX_WITH_OWNERS = gql`
+  query QUERY_TX_WITH_OWNERS($owners: [String!], $tags: [TagFilter!]) {
+    transactions(owners: $owners, tags: $tags, sort: HEIGHT_DESC, first: 1) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          owner {
+            address
+            key
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_TX_BY_ID = gql`
+  query QUERY_TX_BY_ID($id: String!) {
+    transactions(ids: [$id], sort: HEIGHT_DESC, first: 1) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          tags {
+            name
+            value
+          }
+          owner {
+            address
+            key
+          }
+        }
+      }
+    }
+  }
+`;
