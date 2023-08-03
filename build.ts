089a68f731795206dfd28069e481d0b972677f13
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-08-03 11:22:00 +0100

    chore: setup esbuild for multiple platforms

--- build.ts
+++ build.ts
@@ -1,11 +1,35 @@
 import * as esbuild from 'esbuild';
+import { nodeExternalsPlugin } from 'esbuild-node-externals';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
 
 try {
-  await esbuild.build({
-    entryPoints: [ './src/index.ts' ],
+  const sharedConfig = {
+    entryPoints: [ './src/index.ts' ], // entryPoints,
     bundle: true,
     minify: true,
-    outdir: './dist',
+    plugins: [ nodeExternalsPlugin() ],
+    // outdir: 'dist',
+  };
+
+  await esbuild.build({
+    ...sharedConfig,
+    platform: 'node', // for CJS
+    outfile: 'dist/index.js',
+  });
+
+  await esbuild.build({
+    ...sharedConfig,
+    outfile: 'dist/index.esm.js',
+    platform: 'neutral', // for ESM
+    format: 'esm',
+    plugins: sharedConfig.plugins.concat([
+      polyfillNode({
+        // Options (optional)
+        polyfills: {
+          fs: true,
+        }
+      }),
+	  ]),
   });
 } catch (e) {
   console.error(e);
